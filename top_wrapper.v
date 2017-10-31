7b9860b9453d9db194e91debf999c14ef0de036b
Anthony Kuntz <akuntz@cmu.edu>
2017-10-30 21:50:49 -0400

    Changed top_wrapper.v and capstone.xdc
    Updated wrapper and xdc for new connections / signals

--- top_wrapper.v
+++ top_wrapper.v
@@ -2,8 +2,7 @@
 `timescale 1 ps / 1 ps
 
 module top
-   (HDMI_RX_CLK,          // TODO : Which clock? SCLK, MCLK, LRCLK?
-
+   (FMC1_HPC_LA00_CC_P,
     FMC1_HPC_LA22_N,
     FMC1_HPC_LA23_N,
     FMC1_HPC_LA19_N,
@@ -40,33 +39,31 @@ module top
     HDMI_INT,
     hdmi_data,
     hdmi_vsync,
+    ddr3_addr,
+    ddr3_ba,
+    ddr3_cas_n,
+    ddr3_ck_n,
+    ddr3_ck_p,
+    ddr3_cke,
+    ddr3_cs_n,
+    ddr3_dm,
+    ddr3_dq,
+    ddr3_dqs_n,
+    ddr3_dqs_p,
+    ddr3_odt,
+    ddr3_ras_n,
+    ddr3_reset_n,
+    ddr3_we_n,
 
-    // TODO: All these DDR3 below: (Where do these even go?)
-    ddr3_sdram_addr,
-    ddr3_sdram_ba,
-    ddr3_sdram_cas_n,
-    ddr3_sdram_ck_n,
-    ddr3_sdram_ck_p,
-    ddr3_sdram_cke,
-    ddr3_sdram_cs_n,
-    ddr3_sdram_dm,
-    ddr3_sdram_dq,
-    ddr3_sdram_dqs_n,
-    ddr3_sdram_dqs_p,
-    ddr3_sdram_odt,
-    ddr3_sdram_ras_n,
-    ddr3_sdram_reset_n,
-    ddr3_sdram_we_n,
+    iic_scl_io,               // TODO: FIX THESE    
+    iic_sda_io,               // TODO: FIX THESE
 
-    iic_scl_io,               // TODO: ADV7511 example project doesn't have.       
-    iic_sda_io,               //       Where do they go?
+    CPU_RESET,
 
-    reset,                    // TODO: external button?
+    sys_clk_n,
+    sys_clk_p);
 
-    sys_diff_clock_clk_n,     // TODO: What are these?
-    sys_diff_clock_clk_p);
-
-  input  wire HDMI_RX_CLK;
+  input  wire FMC1_HPC_LA00_CC_P;
   input  wire FMC1_HPC_LA22_N;
   input  wire FMC1_HPC_LA23_N;
   input  wire FMC1_HPC_LA19_N;
@@ -108,29 +105,29 @@ module top
   output wire [35:0] hdmi_data;
   output wire hdmi_vsync;
 
-  output wire [13:0] ddr3_sdram_addr;
-  output wire [2:0] ddr3_sdram_ba;
-  output wire ddr3_sdram_cas_n;
-  output wire [0:0] ddr3_sdram_ck_n;
-  output wire [0:0] ddr3_sdram_ck_p;
-  output wire [0:0] ddr3_sdram_cke;
-  output wire [0:0] ddr3_sdram_cs_n;
-  output wire [7:0] ddr3_sdram_dm;
-  inout  wire [63:0] ddr3_sdram_dq;
-  inout  wire [7:0] ddr3_sdram_dqs_n;
-  inout  wire [7:0] ddr3_sdram_dqs_p;
-  output wire [0:0] ddr3_sdram_odt;
-  output wire ddr3_sdram_ras_n;
-  output wire ddr3_sdram_reset_n;
-  output wire ddr3_sdram_we_n;
+  output wire [13:0] ddr3_addr;
+  output wire [2:0] ddr3_ba;
+  output wire ddr3_cas_n;
+  output wire [0:0] ddr3_ck_n;
+  output wire [0:0] ddr3_ck_p;
+  output wire [0:0] ddr3_cke;
+  output wire [0:0] ddr3_cs_n;
+  output wire [7:0] ddr3_dm;
+  inout  wire [63:0] ddr3_dq;
+  inout  wire [7:0] ddr3_dqs_n;
+  inout  wire [7:0] ddr3_dqs_p;
+  output wire [0:0] ddr3_odt;
+  output wire ddr3_ras_n;
+  output wire ddr3_reset_n;
+  output wire ddr3_we_n;
 
   inout  wire iic_scl_io;
   inout  wire iic_sda_io;
 
-  input  wire reset;
+  input  wire CPU_RESET;
 
-  input  wire sys_diff_clock_clk_n;
-  input  wire sys_diff_clock_clk_p;
+  input  wire sys_clk_n;
+  input  wire sys_clk_p;
 
   assign HDMI_RX_P = {FMC1_HPC_LA00_CC_N,
                       FMC1_HPC_LA02_P,
@@ -157,40 +154,37 @@ module top
                       FMC1_HPC_LA20_N,
                       FMC1_HPC_LA19_P}
 
-
-  hdmi_passthrough_wrapper design(.HDMI_RX_CLK(HDMI_RX_CLK),
-                                  .HDMI_RX_DE(FMC1_HPC_LA22_N),
-                                  .HDMI_RX_HP(FMC1_HPC_LA23_N),
-                                  .HDMI_RX_HSYNC(FMC1_HPC_LA19_N),
-                                  .HDMI_RX_INT1(FMC1_HPC_LA25_N),
-                                  .HDMI_RX_P(HDMI_RX_P),
-                                  .HDMI_RX_RESETN(FMC1_HPC_LA23_P),
-                                  .HDMI_RX_VSYNC(FMC1_HPC_LA22_P),
-                                  .HDMI_TX_CLK(hdmi_out_clk),
-                                  .HDMI_TX_DE(hdmi_data_e),
-                                  .HDMI_TX_HSYNC(hdmi_hsync),
-                                  .HDMI_TX_INT(HDMI_INT),
-                                  .HDMI_TX_P(hdmi_data[23:0]),
-                                  .HDMI_TX_VSYNC(hdmi_vsync),
-                                  .ddr3_sdram_addr(ddr3_sdram_addr),
-                                  .ddr3_sdram_ba(ddr3_sdram_ba),
-                                  .ddr3_sdram_cas_n(ddr3_sdram_cas_n),
-                                  .ddr3_sdram_ck_n(ddr3_sdram_ck_n),
-                                  .ddr3_sdram_ck_p(ddr3_sdram_ck_p),
-                                  .ddr3_sdram_cke(ddr3_sdram_cke),
-                                  .ddr3_sdram_cs_n(ddr3_sdram_cs_n),
-                                  .ddr3_sdram_dm(ddr3_sdram_dm),
-                                  .ddr3_sdram_dq(ddr3_sdram_dq),
-                                  .ddr3_sdram_dqs_n(ddr3_sdram_dqs_n),
-                                  .ddr3_sdram_dqs_p(ddr3_sdram_dqs_p),
-                                  .ddr3_sdram_odt(ddr3_sdram_odt),
-                                  .ddr3_sdram_ras_n(ddr3_sdram_ras_n),
-                                  .ddr3_sdram_reset_n(ddr3_sdram_reset_n),
-                                  .ddr3_sdram_we_n(ddr3_sdram_we_n),
-                                  .iic_scl_io(iic_scl_io),
-                                  .iic_sda_io(iic_sda_io),
-                                  .reset(reset),
-                                  .sys_diff_clock_clk_n(sys_diff_clock_clk_n),
-                                  .sys_diff_clock_clk_p(sys_diff_clock_clk_p));
+  hdmi_tx_rx_wrapper design(.HDMI_CLK(FMC1_HPC_LA00_CC_P),
+                            .HDMI_RESET(FMC1_HPC_LA23_P), // RESETN?
+                            .HDMI_RX_DE(FMC1_HPC_LA22_N),
+                            .HDMI_RX_HPA(FMC1_HPC_LA23_N),
+                            .HDMI_RX_HSYNC(FMC1_HPC_LA19_N),
+                            .HDMI_RX_P(HDMI_RX_P),
+                            .HDMI_RX_VSYNC(FMC1_HPC_LA22_P),
+                            .HDMI_TX_CLK(hdmi_out_clk),
+                            .HDMI_TX_DE(hdmi_data_e),
+                            .HDMI_TX_HSYNC(hdmi_hsync),
+                            .HDMI_TX_P(hdmi_data[23:0]),
+                            .HDMI_TX_VSYNC(hdmi_vsync),
+                            .ddr3_sdram_addr(ddr3_addr),
+                            .ddr3_sdram_ba(ddr3_ba),
+                            .ddr3_sdram_cas_n(ddr3_cas_n),
+                            .ddr3_sdram_ck_n(ddr3_ck_n),
+                            .ddr3_sdram_ck_p(ddr3_ck_p),
+                            .ddr3_sdram_cke(ddr3_cke),
+                            .ddr3_sdram_cs_n(ddr3_sdram_cs_n),
+                            .ddr3_sdram_dm(ddr3_dm),
+                            .ddr3_sdram_dq(ddr3_dq),
+                            .ddr3_sdram_dqs_n(ddr3_dqs_n),
+                            .ddr3_sdram_dqs_p(ddr3_dqs_p),
+                            .ddr3_sdram_odt(ddr3_odt),
+                            .ddr3_sdram_ras_n(ddr3_ras_n),
+                            .ddr3_sdram_reset_n(ddr3_reset_n),
+                            .ddr3_sdram_we_n(ddr3_we_n),
+                            .iic_scl_io(iic_scl_io),
+                            .iic_sda_io(iic_sda_io),
+                            .reset(CPU_RESET),
+                            .sys_diff_clock_clk_n(sys_clk_n),
+                            .sys_diff_clock_clk_p(sys_clk_p));
 
 endmodule // top
